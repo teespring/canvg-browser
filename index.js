4c25ca02762f504b42728b71257431ec77c4dad5
Ricardo Matias <ricardo.matias0@gmail.com>
2016-03-15 11:29:42 +0100

    chore(project): prepare for npm publishing

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
- var RGBColor = require("rgbcolor");
- var stackblur = require("stackblur");
+ var RGBColor = require('rgbcolor');
+ var stackblur = require('stackblur');
  var xmldom = require('xmldom');
 
- /*
-  * canvg.js - Javascript SVG parser and renderer on Canvas
-  * MIT Licensed
-  * Gabe Lerner (gabelerner@gmail.com)
-  * http://code.google.com/p/canvg/
-  *
-  * Requires: rgbcolor.js - http://www.phpied.com/rgb-color-parser-in-javascript/
-  */
+/*
+ * canvg.js - Javascript SVG parser and renderer on Canvas
+ * MIT Licensed
+ * Gabe Lerner (gabelerner@gmail.com)
+ * http://code.google.com/p/canvg/
+ *
+ * Requires: rgbcolor.js - http://www.phpied.com/rgb-color-parser-in-javascript/
+ */
 
 /*
 canvg(target, s)
