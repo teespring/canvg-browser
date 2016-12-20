832d6cae8be4fcded6ae9b23741e6857fbdec689
Nico Rehwaldt <git_nikku@nixis.de>
2016-12-20 16:38:11 +0100

    test(markers): verify correct marker export

--- test/canvg-spec.js
+++ test/canvg-spec.js
@@ -5,22 +5,28 @@ var fs = require('fs');
 var canvg = require('../');
 
 var basicSVG = require('./fixtures/basic.svg'),
-    complexSVG = require('./fixtures/complex.svg');
+    complexSVG = require('./fixtures/complex.svg'),
+    markerSVG = require('./fixtures/marker.svg');
 
 
 describe('canvg', function () {
+
   var canvas;
 
   beforeEach(function() {
     canvas = document.createElement('canvas');
+
+    document.body.appendChild(canvas);
   });
+
 
   it('should create canvas from a basic svg', function() {
     // when
     canvg(canvas, basicSVG);
 
     // then
-    expect(canvas.outerHTML).to.equal('<canvas width="373" height="99" style="width: 373px; height: 99px;"></canvas>');
+    expect(canvas.outerHTML).to.equal(
+      '<canvas width="373" height="99" style="width: 373px; height: 99px;"></canvas>');
   });
 
 
@@ -29,7 +35,18 @@ describe('canvg', function () {
     canvg(canvas, complexSVG);
 
     // then
-    expect(canvas.outerHTML).to.equal('<canvas width="5134" height="1834" style="width: 5134px; height: 1834px;"></canvas>');
+    expect(canvas.outerHTML).to.equal(
+      '<canvas width="5134" height="1834" style="width: 5134px; height: 1834px;"></canvas>');
+  });
+
+
+  it('should create canvas with marker', function() {
+    // when
+    canvg(canvas, markerSVG);
+
+    // then
+    expect(canvas.outerHTML).to.equal(
+      '<canvas width="234" height="92" style="width: 234px; height: 92px;"></canvas>');
   });
 
 
